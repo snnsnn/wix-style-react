e10671be60ef56f55c280465328e0dc331bb0764
Shlomi Toussia-Cohen <shlomitc@wix.com>
2019-05-08 00:14:47 +0300

    minor

--- src/Badge/Badge.protractor.driver.js
+++ src/Badge/Badge.protractor.driver.js
@@ -1,6 +1,8 @@
-export const badgeDriverFactory = component => ({
+const badgeDriverFactory = component => ({
   /** returns the component element */
   element: () => component,
   /** returns the component text */
   text: async () => component.getText(),
 });
+
+export default badgeDriverFactory;
