a6b05239adda0dbb7783379a5e60eb8841c858f8
Hadar Geva <hadarge@wix.com>
2019-05-15 10:58:38 +0300

    WIP

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,1 +1,19 @@
-export { default } from './Sidebar.js';
+import sidebar from './Sidebar';
+import { SidebarItemContext, SidebarContext } from './SidebarAPI';
+import { SidebarBackButton } from './BackButton/SidebarBackButton';
+import { SidebarItem } from './SidebarItem/SidebarItem';
+import { PersistentFooter } from './SidebarItem/PersistentFooter';
+import { PersistentHeader } from './SidebarItem/PersistentHeader';
+import { TextView } from './SidebarItem/TextView/TextView';
+
+export default sidebar;
+
+export {
+  SidebarItemContext,
+  SidebarContext,
+  SidebarBackButton,
+  SidebarItem,
+  PersistentFooter,
+  PersistentHeader,
+  TextView,
+};
