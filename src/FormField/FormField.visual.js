fb979129a1c2eeac2ade8146cc821c70003e7f5f
Gil Meir <gilme@wix.com>
2019-05-26 16:59:01 +0300

    change visual test code according to convention

--- src/FormField/FormField.visual.js
+++ src/FormField/FormField.visual.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import FormField from './FormField';
+import FormFieldComponent from './FormField';
 import Input from '../Input';
 
-storiesOf(`FormField`, module).add('Label ellipsis', () => (
-  <div style={{ width: 285, padding: 50 }}>
-    <FormField
-      infoContent="something"
-      label="a long label that should use ellipsis, but if you see the whole sentence, something probably doesn't work well"
-    >
-      <Input />
-    </FormField>
-  </div>
-));
+const FormField = props => {
+  return (
+    <div style={{ width: 285, padding: 50 }}>
+      <FormFieldComponent {...props}>
+        <Input />
+      </FormFieldComponent>
+    </div>
+  );
+};
+
+const test = (it, props) => ({ it, props });
+
+const tests = [
+  {
+    describe: 'Label',
+    its: [
+      test('Ellipsis', {
+        label:
+          "a long label that should use ellipsis, but if you see the whole sentence, something probably doesn't work well",
+      }),
+      test('Info Content', {
+        infoContent: 'hi',
+      }),
+    ],
+  },
+];
+
+tests.forEach(({ describe, its }) => {
+  its.forEach(({ it, props }) => {
+    storiesOf(`FormField/${describe}`, module).add(it, () => (
+      <FormField {...props} />
+    ));
+  });
+});
