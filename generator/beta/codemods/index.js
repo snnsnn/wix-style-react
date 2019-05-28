c8dd21d9ac317633fbb2d0e43168c49527b9b9f9
mykas <mykolass@wix.com>
2019-05-28 15:51:00 +0300

    Generator - add generator for beta folder

---
+++ generator/beta/codemods/index.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    codemod: 'index-file.js',
+    dist: 'src/beta/index.js',
+    description: 'Add component export to the index file',
+  },
+  {
+    codemod: 'testkit-definitions.js',
+    dist: 'testkit/testkit-definitions-beta.js',
+    description: 'Update testkit-definitions-beta.js file',
+  },
+];
