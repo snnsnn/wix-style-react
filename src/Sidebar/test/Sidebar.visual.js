b065a5b29fcedbf7c8e29b5aaf0374fa86bc015f
Hadar Geva <hadarge@wix.com>
2019-05-30 11:53:48 +0300

    WIP

--- src/Sidebar/test/Sidebar.visual.js
+++ src/Sidebar/test/Sidebar.visual.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Sidebar } from '../Sidebar';
+import Sidebar from '../Sidebar';
+import {
+  PersistentFooter,
+  PersistentHeader,
+  SidebarBackButton,
+  SidebarItem,
+  TextView,
+} from '..';
 
 const commonProps = {
   //use for repeated props across the tests (e.g. {buttonText: 'example'})
@@ -23,7 +30,46 @@ const tests = [
 tests.forEach(({ describe, its }) => {
   its.forEach(({ it, props }) => {
     storiesOf(`Sidebar/${describe}`, module).add(it, () => (
-      <Sidebar {...commonProps} {...props} />
+      <Sidebar dataHook={'sidebar-data-hook'} selectedKey={'item1'}>
+        <PersistentHeader>
+          <div
+            style={{ textAlign: 'center', fontSize: '20px', margin: '10px' }}
+          >
+            Simple Sidebar
+          </div>
+        </PersistentHeader>
+
+        <SidebarItem
+          itemKey={'item1'}
+          innerMenu={[
+            <SidebarBackButton>
+              <TextView>Back</TextView>
+            </SidebarBackButton>,
+            <SidebarItem itemKey={'item4'}>
+              <TextView>Inner Item 1</TextView>
+            </SidebarItem>,
+            <SidebarItem itemKey={'item5'}>
+              <TextView>Inner Item 2</TextView>
+            </SidebarItem>,
+          ]}
+        >
+          <TextView>Item 1</TextView>
+        </SidebarItem>
+        <SidebarItem itemKey={'item2'} disable="true">
+          <TextView disable="true">Item 2</TextView>
+        </SidebarItem>
+        <SidebarItem itemKey={'item3'}>
+          <TextView>Item 3</TextView>
+        </SidebarItem>
+
+        <PersistentFooter>
+          <div
+            style={{ textAlign: 'center', fontSize: '20px', margin: '10px' }}
+          >
+            Sidebar Footer
+          </div>
+        </PersistentFooter>
+      </Sidebar>
     ));
   });
 });
